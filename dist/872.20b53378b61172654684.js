7e8f224b1dfd53da752ea1baa3f230b3df1dc436
Vova <tarabanvv@mail.ru>
2024-12-11 13:38:06 +0800

    Добавил react router и react lazy

---
+++ dist/872.20b53378b61172654684.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkulbiblog=self.webpackChunkulbiblog||[]).push([[872],{872:(u,e,l)=>{l.r(e),l.d(e,{default:()=>n});var b=l(848);const n=function(){return(0,b.jsx)("div",{children:"AboutPage"})}}}]);
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import './index.scss';
-import { Counter } from './components/Counter';
+import { Link, Route, Routes } from 'react-router-dom';
+import { MainPageAsync } from './pages/MainPage/MainPage.async';
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
+import { Suspense } from 'react';
 
 const App = () => {
 	return (
 		<div className='app'>
-			adfgdfgd
-			<Counter />
+			<Link to={'/'}>Главная</Link>
+			<Link to={'/about'}>О сайте</Link>
+			<Suspense fallback={<div>Loading...</div>}>
+				<Routes>
+					<Route path={'/'} element={<MainPageAsync />} />
+					<Route path={'/about'} element={<AboutPageAsync />} />
+				</Routes>
+			</Suspense>
 		</div>
 	);
 };
